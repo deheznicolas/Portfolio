cb738a4b8475ddfa398e67c546ae41e2ea4dc954
EnricDul <enric.dulieu97@gmail.com>
2020-01-27 09:27:08 +0100

    New design and animation

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio
-Un voyage dans l'espace qui vous mènera aux projets que j'ai accompli durant mon parcours 
--- script/script.js
+++ script/script.js
@@ -1,18 +1,19 @@
-$(function () {
-  /**
-  * Smooth scrolling to page anchor on click
-  **/
-  $("a[href*='#']:not([href='#'])").click(function () {
-    if (
-      location.hostname == this.hostname
-      && this.pathname.replace(/^\//, "") == location.pathname.replace(/^\//, "")
-    ) {
-      var anchor = $(this.hash);
-      anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) + "]");
-      if (anchor.length) {
-        $("html, body").animate({ scrollTop: anchor.offset().top }, 1000);
-      }
-    }
-  });
+// //Smooth scrolling while clicking the custom button
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'slow'
+    });
+  });
 });
+
+
+
+
+
+
+
+
