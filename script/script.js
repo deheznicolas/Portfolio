685692529e518e0bc41fd326b23f9ce5382308a1
EnricDul <enric.dulieu97@gmail.com>
2020-01-27 10:19:44 +0100

    hide and show dynamic content / script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-// //Smooth scrolling while clicking the custom button
+//Smooth scrolling while clicking the custom button
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
@@ -9,6 +9,41 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+//Vertical menu section 1 | show and hide content.
+
+let story = document.getElementById("story");
+let dev = document.getElementById("dev");
+let design = document.getElementById("design");
+
+story.style.display = 'block';
+dev.style.display = 'none';
+design.style.display = 'none';
+
+
+document.getElementById("story-btn").addEventListener('click', () => {
+  if (dev.style.display == 'block' || design.style.display == 'block') {
+    dev.style.display = 'none';
+    design.style.display = 'none';
+    story.style.display = 'block';
+  }
+})
+document.getElementById("dev-btn").addEventListener('click', () => {
+  if (story.style.display == 'block' || design.style.display == 'block') {
+    story.style.display = 'none';
+    design.style.display = 'none';
+    dev.style.display = 'block';
+  }
+})
+
+document.getElementById("design-btn").addEventListener('click', () => {
+  if (dev.style.display == 'block' || story.style.display == 'block') {
+    dev.style.display = 'none';
+    story.style.display = 'none';
+    design.style.display = 'block';
+
+  }
+})
 
 
 
--- script/starstransition.js
+++ script/starstransition.js
@@ -1,104 +1,104 @@
-const canvas = document.getElementById("mycanvas");
-const c = canvas.getContext("2d");
-let width;
-let height;
+// const canvas = document.getElementById("mycanvas");
+// const c = canvas.getContext("2d");
+// let width;
+// let height;
 
-//Responsive Canvas for different screen size
-
-
-const canvasToResponsive = () => {
-  width = document.getElementById("star-transition").clientWidth;
-  height = document.getElementById("star-transition").clientHeight;
-  canvas.width = width;
-  canvas.height = height;
-};
-
-canvasToResponsive();
-
-window.onresize = () => {
-  canvasToResponsive();
-};
+// //Responsive Canvas for different screen size
 
 
+// const canvasToResponsive = () => {
+//   width = document.getElementById("star-transition").clientWidth;
+//   height = document.getElementById("star-transition").clientHeight;
+//   canvas.width = width;
+//   canvas.height = height;
+// };
+
+// canvasToResponsive();
+
+// window.onresize = () => {
+//   canvasToResponsive();
+// };
 
 
-//Building stars in the field
-
-const makeStars = count => {
-  const out = [];
-  for (let i = 0; i < count; i++) {
-    const s = {
-      x: Math.random() * 1600 - 800,
-      y: Math.random() * 900 - 450,
-      z: Math.random() * 10000
-    };
-    out.push(s);
-  }
-  return out;
-};
-
-let stars = makeStars(10000);
-
-const clear = () => {
-  c.fillStyle = "black";
-  c.fillRect(0, 0, canvas.width, canvas.height);
-};
-
-const putPixel = (x, y, brightness) => {
-  const intensity = brightness * 255;
-  const rgb = "rgb(" + intensity + "," + intensity + "," + intensity + ")";
-  c.fillStyle = rgb;
-  c.fillRect(x, y, 1, 1); //Utiliser drawCircle pour générer des ronds des x pixel de rayon, pour avoir des étoiles de différentes tailles
-};
 
 
-//Moving stars from template
-const moveStars = distance => {
-  const count = stars.length;
-  for (var i = 0; i < count; i++) {
-    const s = stars[i];
-    s.z -= distance;
-    while (s.z <= 1) {
-      s.z += 1;
-    }
-  }
-};
+// //Building stars in the field
 
-let prevTime;
-const init = time => {
-  prevTime = time;
-  requestAnimationFrame(tick);
-};
+// const makeStars = count => {
+//   const out = [];
+//   for (let i = 0; i < count; i++) {
+//     const s = {
+//       x: Math.random() * 1600 - 800,
+//       y: Math.random() * 900 - 450,
+//       z: Math.random() * 10000
+//     };
+//     out.push(s);
+//   }
+//   return out;
+// };
 
-const tick = time => {
-  let elapsed = time - prevTime;
-  prevTime = time;
+// let stars = makeStars(10000);
 
-  moveStars(elapsed * 0.05);
+// const clear = () => {
+//   c.fillStyle = "black";
+//   c.fillRect(0, 0, canvas.width, canvas.height);
+// };
 
-  clear();
+// const putPixel = (x, y, brightness) => {
+//   const intensity = brightness * 255;
+//   const rgb = "rgb(" + intensity + "," + intensity + "," + intensity + ")";
+//   c.fillStyle = rgb;
+//   c.fillRect(x, y, 1, 1); //Utiliser drawCircle pour générer des ronds des x pixel de rayon, pour avoir des étoiles de différentes tailles
+// };
 
-  const cx = width / 2;
-  const cy = height / 2;
 
-  const count = stars.length;
-  for (var i = 0; i < count; i++) {
-    const star = stars[i];
+// //Moving stars from template
+// const moveStars = distance => {
+//   const count = stars.length;
+//   for (var i = 0; i < count; i++) {
+//     const s = stars[i];
+//     s.z -= distance;
+//     while (s.z <= 1) {
+//       s.z += 1;
+//     }
+//   }
+// };
 
-    const x = cx + star.x / (star.z * 0.001);
-    const y = cy + star.y / (star.z * 0.001);
+// let prevTime;
+// const init = time => {
+//   prevTime = time;
+//   requestAnimationFrame(tick);
+// };
 
-    if (x < 0 || x >= width || y < 0 || y >= height) {
-      continue;
-    }
+// const tick = time => {
+//   let elapsed = time - prevTime;
+//   prevTime = time;
 
-    const d = star.z / 1000.0;
-    const b = 1 - d * d;
+//   moveStars(elapsed * 0.05);
 
-    putPixel(x, y, b);
-  }
+//   clear();
 
-  requestAnimationFrame(tick);
-};
+//   const cx = width / 2;
+//   const cy = height / 2;
 
-requestAnimationFrame(init);
+//   const count = stars.length;
+//   for (var i = 0; i < count; i++) {
+//     const star = stars[i];
+
+//     const x = cx + star.x / (star.z * 0.001);
+//     const y = cy + star.y / (star.z * 0.001);
+
+//     if (x < 0 || x >= width || y < 0 || y >= height) {
+//       continue;
+//     }
+
+//     const d = star.z / 1000.0;
+//     const b = 1 - d * d;
+
+//     putPixel(x, y, b);
+//   }
+
+//   requestAnimationFrame(tick);
+// };
+
+// requestAnimationFrame(init);
